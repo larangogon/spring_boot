3f0c73116040d3c1e2988a624dade2a45368e95c
larangogon <67110144+larangogon@users.noreply.github.com>
2022-12-16 22:10:31 -0500

    feat: add model view

--- src/main/java/com/movie/app/controller/movieDB.java
+++ src/main/java/com/movie/app/controller/movieDB.java
@@ -1,10 +1,13 @@
 package com.movie.app.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import info.movito.themoviedbapi.TmdbApi;
@@ -22,7 +25,7 @@ public class movieDB {
     //https://api.themoviedb.org/3/movie/550?api_key=cad0d95925c6869c60ed49f550af6c90
 	//buscando pelicula por id
 	@GetMapping("/{movieId}")
-	public MovieDb getMovieInfoDB(@PathVariable("movieId") Integer movieId) {
+	public ModelAndView getMovieInfoDB(@PathVariable("movieId") Integer movieId) {
 
         LOG.info("Detail of the movie in api bd...");
 
@@ -30,20 +33,29 @@ public class movieDB {
 
 		MovieDb movie = movies.getMovie(movieId, "en");
 
-        return movie;
+		ModelAndView mav = new ModelAndView("show");
+
+		mav.addObject("movie", movie);
+        return mav;
 	}
 
 	//https://api.themoviedb.org/3/movie/popular?api_key=cad0d95925c6869c60ed49f550af6c90
 	//populares
-	@GetMapping("/getAllMovies")
-	public MovieResultsPage getAllMovies() {
+	@GetMapping("/getPopularMovies")
+	public String getPopularMovies(
+		@RequestParam(required = false, defaultValue = "0") Integer page,
+		@RequestParam(required = false, defaultValue = "20") Integer size,
+		@RequestParam(required = false, defaultValue = "false") Boolean enablePagination,
+		Model model
+	) {
 		LOG.info("All the movies in api bd...");
 
 		TmdbMovies movies = new TmdbApi("<apikey>").getMovies();
 
-		MovieResultsPage movie = movies.getPopularMovies(null, null);
+		MovieResultsPage moviesPMovieResultsPage = movies.getPopularMovies(null, null);
 
-        return movie;
+		model.addAttribute("movies", moviesPMovieResultsPage);
+
+        return "index";
 	}
-
 }
